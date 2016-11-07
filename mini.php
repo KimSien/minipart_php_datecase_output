00321c74814cca92327d6ff39e2329d91e52fd44
sei0044 <sei0044@gmail.com>
2016-11-07 12:14:45 +0900

    modify mini.php

--- mini.php
+++ mini.php
@@ -1,12 +1,16 @@
 <?php
+/**
+* 変数汚染用にnamespaceで対応
+*/
 namespace changingdisplay;
+
 /**
 *　設定関係
 */
 date_default_timezone_set('Asia/Tokyo');
 $jsonurl = "settingdata.json"; // json url
-$baseimageurl = "base.jpg"; // base jpeg
-$basicalt="基本オルト";
+
+
 /**
 * 現在の時間を取得
 */
@@ -15,6 +19,7 @@ $json = file_get_contents($jsonurl);
 
 // JSON($json)を連想配列に変換(デコード)する
 $setting = json_decode( $json , true ) ;
+
 
 /**
 * 期間内かどうか確かめて出力
@@ -28,9 +33,12 @@ foreach ($setting as $value) {
      $alt = $value["alt"];
      break;
     } else {
-    $image_url = $baseimageurl;
-    $alt=$basicalt;
-    //echo " out";   
+        //base setgings
+        if(!isset($image_url)){
+        $image_url = $setting["base"]["imgurl"];
+        $alt=$setting["base"]["alt"];
+        echo "repeat";
+        }
     }
 }
 
