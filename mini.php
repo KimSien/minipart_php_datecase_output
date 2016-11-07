0b154881f24c8f8ea03989f5402e4323b7d86fa8
sei0044 <sei0044@gmail.com>
2016-11-07 11:51:10 +0900

    changing json

--- mini.php
+++ mini.php
@@ -1,36 +1,42 @@
 <?php
 namespace changingdisplay;
-date_default_timezone_set('Asia/Tokyo');
 /**
-*
+*　設定関係
+*/
+date_default_timezone_set('Asia/Tokyo');
+$jsonurl = "settingdata.json"; // json url
+$baseimageurl = "base.jpg"; // base jpeg
+$basicalt="基本オルト";
+/**
+* 現在の時間を取得
 */
 $current = date('Y-m-d H:i:s');
-$setting = [
-    "2016-11-7 10:00:0"=>"**.jpg",
-    "2016-11-14 10:0:0"=>"**.jpg"
+$json = file_get_contents($jsonurl);
 
-];
-
+// JSON($json)を連想配列に変換(デコード)する
+$setting = json_decode( $json , true ) ;
 
 /**
-*
+* 期間内かどうか確かめて出力
 */
-foreach ($setting as $key => $value) {
-
+foreach ($setting as $value) {
     /**
     * 期間内かどうかを確かめる
-    */
-    if (strtotime($current) < strtotime($key)) {
-     $image_url = $value;
+    */   
+    if (strtotime($current) < strtotime($value["date"])) {
+     $image_url = $value["imgurl"];
+     $alt = $value["alt"];
      break;
     } else {
+    $image_url = $baseimageurl;
+    $alt=$basicalt;
     //echo " out";   
     }
-
 }
 
+/**
+* 出力
+*/
 echo <<< EOM
-
-<img src="{$image_url}" >
-
+<img src="{$image_url}" alt="{$alt}">
 EOM;
